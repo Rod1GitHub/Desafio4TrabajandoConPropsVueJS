e3c60ae3b304404744cb0587906515b5cdd5c9ad
Rod <rod@Rods-MacBook-Pro.local>
2022-01-29 00:21:30 -0300

    first commit

---
+++ desafiotrabajandoconprops/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app-trabajando-con-props">
+    <h2>Tarea</h2>
+    <input type="text" v-model="taskSingularInput006" />
+    <button @click="captureInputAndAddToEndOfArrayMethod006">
+      Capturar y agregar al array
+    </button>
+    <ToDoListComponent006 v-bind:tasksPluralArray006="tasksPluralArray006" />
+  </div>
+</template>
+
+<script>
+import ToDoListComponent006 from "@/components/ToDoList.vue";
+export default {
+  name: "App",
+  components: {
+    ToDoListComponent006,
+  },
+  data() {
+    return {
+      taskSingularInput006: "",
+      tasksPluralArray006: [],
+    };
+  },
+  methods: {
+    captureInputAndAddToEndOfArrayMethod006() {
+      let { taskSingularInput006 } = this; // Esto captura el input
+      this.tasksPluralArray006.push(taskSingularInput006); // push() lo envía al final del array
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Arial;
+}
+</style>
---
+++ desafiotrabajandoconprops/src/components/ToDoList.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+    <li
+      v-for="(taskArgument006, index006) in tasksPluralArray006"
+      :key="index006"
+    >
+      <button @click="deleteTaskfromArrayMethod(index006)">
+        Borrar del array
+      </button>
+      {{ taskArgument006 }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "ToDoList",
+  props: {
+    tasksPluralArray006: [],
+  },
+  methods: {
+    deleteTaskfromArrayMethod(indexParam) {
+      this.tasksPluralArray006.splice(indexParam, 1);
+    },
+  },
+};
+</script>
+
+<style></style>
